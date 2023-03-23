f6071be26cb213a299a10da9f1f79abdfe814d52
fendishatech <liyuknow@gmail.com>
2023-03-22 21:33:11 -0700

    starter template using

---
+++ src/views/index.jsx
@@ -0,0 +1,6 @@
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Layout from "./public/layout";
+import Home from "./public/home/";
+
+export { Login, Register, Layout, Home };
